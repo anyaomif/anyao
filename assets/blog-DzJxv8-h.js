23f6eb0f2396d5d92ea760a13732c6587c5152c0
smallAnYao <206788568@qq.com>
2024-10-21 14:54:15 +0800

    Initial commit

---
+++ assets/blog-DzJxv8-h.js
@@ -0,0 +1,1 @@
+import{r as e}from"./request-BHdVL5DQ.js";class a{static getCategories(){return e({url:"/categories",method:"get"})}static getBlogById(t){return e({url:"/articles",method:"get",params:{id:t}})}}export{a as B};
---
+++ assets/home-Dr0ghUkJ.js
@@ -0,0 +1,1 @@
+import{_ as e,c,d as n}from"./index-B9PLwloD.js";const o={};function r(t,s){return c(),n("div",null," 首页 ")}const a=e(o,[["render",r]]);export{a as default};
